648b5ccabd51a0e5f730a4012392be2abfc0015c
Johan Adell <johan.adell@maxiv.lu.se>
2016-03-14 08:03:29 +0100

    Bugfix: Call nxload to not clash with load from numpy

--- helper/SpectrumFile.py
+++ helper/SpectrumFile.py
@@ -1,4 +1,4 @@
-from nexpy.api import nexus as nx
+from ext.nexpy.api import nexus as nx
 from model.ArpesData import *
 from helper.converter import Arpes2DSpectrumConverter
 
@@ -10,7 +10,7 @@ class SpectrumFile:
 		extension = filepath.split('.')[-1]
 		if extension == "nxs":
 			try:
-				loadeddata = nx.load(filepath,'r')
+				loadeddata = nx.nxload(filepath,'r')
 				## Check if data is ARPES, right now just load!
 				if len(loadeddata.entries) != 1:		# How many entries do we have in the nexus file
 					data = ArpesData(loadeddata, 0)		# Right now, just load the first one
